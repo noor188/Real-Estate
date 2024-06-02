d3ef8234eca35469f18119abdde744b2d22523a6
Noor <noormmlk@gmail.com>
2024-06-02 16:15:45 -0600

    Created 5 test cases and the set_squareFootage method

--- app/property.py
+++ app/property.py
@@ -15,11 +15,24 @@ class Property:
     '''
     ## ====================
     ## functions
-    def __init__(self, squareFootage: float, numofBedrooms: int, numofBathrooms: int) -> None:
-        self.squareFootage  = squareFootage
-        self.numofBedrooms  = numofBedrooms
-        self.numofBathrooms = numofBathrooms      
-    
+    class Property: # stub
+        def __init__(self, squareFootage: float, numofBedrooms: int = 0, numofBathrooms: int= 1) -> None:
+            self.set_squareFootage(squareFootage)
+            self.numofBedrooms  = numofBedrooms
+            self.numofBathrooms = numofBathrooms      
+
+        def set_squareFootage(self, squareFootage):            
+            try:
+                if isinstance(squareFootage, str) or isinstance(squareFootage, bool): # string or boolean
+                    raise TypeError
+                if squareFootage < 0 : # neg
+                    raise ValueError    
+            except TypeError: # loggin the error, notifying the user, reverting to a defult value or other appropriate actions 
+                raise TypeError
+            except ValueError:
+                raise ValueError 
+            else:
+                self.squareFootage = squareFootage
     """
     def display(self) -> None:
         ''' display the property data values'''       
