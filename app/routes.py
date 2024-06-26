6d70765999cba2a0e6e9fe107086159d9a20bb09
Noor <noormmlk@gmail.com>
2024-06-26 15:33:41 -0600

    adding templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello world!"
+    '''View function for the inde web page'''
+    user = {'username': 'Noor'}
+    return render_template("index.html", title= "Microblog", user= user)
