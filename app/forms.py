4ae418e00721527c8950e65ed6b67bb34a81ed83
Noor <noormmlk@gmail.com>
2024-07-31 15:37:22 -0600

    Domain analysis for the property home page

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 
